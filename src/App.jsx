fe9a3de965f62620dc38c2bb6ef53cbffee1b3e0
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2026-02-17 07:46:23 +0100

    sds

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import AdminDashboard from "./pages/AdminDashboard";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -16,7 +16,7 @@ function App() {
         <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </Router>
   );
 }
 
